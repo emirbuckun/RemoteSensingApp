00526b632e6a8eb864c30a8418d63918e8606110
Emir Büçkün <emirbuckun@hotmail.com>
2024-01-03 19:57:07 +0300

    Sensors' programs are added

--- RemoteSensingApp.SensorHum/Program.cs
+++ RemoteSensingApp.SensorHum/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// Set UDP connection
+IPEndPoint ipEndPoint = new(IPAddress.Parse("127.0.0.1"), 8082);
+using Socket client = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+await client.ConnectAsync(ipEndPoint);
+
+// Send message
+int aliveCounter = 0;
+while (true)
+{
+    DateTime dateTime = DateTime.Now;
+
+    // Send alive message
+    if (aliveCounter >= 3)
+    {
+        var aliveMsg = "Humidity Sensor: ALIVE Date: " + dateTime.ToString();
+        var aliveMsgBytes = Encoding.UTF8.GetBytes(aliveMsg);
+        _ = await client.SendAsync(aliveMsgBytes, SocketFlags.None);
+
+        // Print sent data
+        Console.WriteLine("Humidity Sensor Sent Data: " + aliveMsg);
+        aliveCounter = 0;
+    }
+
+    // Create humidity data btw 40-90
+    Random random = new();
+    int humidity = random.Next(40, 90);
+
+    // Send humidity data
+    if (humidity > 80)
+    {
+        var message = "Humidity Sensor: " + humidity.ToString() + " Date: " + dateTime.ToString();
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        _ = await client.SendAsync(messageBytes, SocketFlags.None);
+
+        // Print sent data
+        Console.WriteLine("Humidity Sensor Sent Data: " + message);
+    }
+
+    // Wait for a second
+    Thread.Sleep(1000);
+
+    // Increment alive message counter
+    aliveCounter++;
+}
--- RemoteSensingApp.SensorTemp/Program.cs
+++ RemoteSensingApp.SensorTemp/Program.cs
@@ -2,24 +2,27 @@
 using System.Net.Sockets;
 using System.Text;
 
-IPEndPoint ipEndPoint = new(IPAddress.Any, 8081);
+// Set TCP connection
+IPEndPoint ipEndPoint = new(IPAddress.Parse("127.0.0.1"), 8081);
 using Socket client = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
 await client.ConnectAsync(ipEndPoint);
 
 // Send message
 while (true)
 {
-    // Create random number btw 20-30
+    // Create temperature data btw 20-30
     Random random = new();
-    int randomInt = random.Next(20, 30);
-
+    int temperature = random.Next(20, 30);
     DateTime dateTime = DateTime.Now;
 
-    var message = "Temperature Sensor: " + randomInt.ToString() + "°C Date: " + dateTime.ToString();
+    // Send temperature data
+    var message = "Temperature Sensor: " + temperature.ToString() + "°C Date: " + dateTime.ToString();
     var messageBytes = Encoding.UTF8.GetBytes(message);
     _ = await client.SendAsync(messageBytes, SocketFlags.None);
 
+    // Print sent data
     Console.WriteLine("Temperature Sensor Sent Data: " + message);
+
+    // Wait for a second
     Thread.Sleep(1000);
 }
