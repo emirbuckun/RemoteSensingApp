79a1ee72034e926c5b5bf127095924a50c889c5c
Emir Büçkün <emirbuckun@hotmail.com>
2024-01-04 10:09:41 +0300

    Comment added

--- RemoteSensingApp.Gateway/Program.cs
+++ RemoteSensingApp.Gateway/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Net;
 using RemoteSensingApp.Gateway;
 
-IPEndPoint ipEndPointTemp = new(IPAddress.Parse("127.0.0.1"), 8081);
-IPEndPoint ipEndPointHum = new(IPAddress.Parse("127.0.0.1"), 8082);
+// Arrange sensor endpoints ("127.0.0.1" = localhost)
+IPEndPoint ipEndPointTemp = new(IPAddress.Parse("127.0.0.1"), 8081); // Temperature sensor
+IPEndPoint ipEndPointHum = new(IPAddress.Parse("127.0.0.1"), 8082); // Humidity sensor
 
 IPEndPoint[] ipPoints = new IPEndPoint[2] { ipEndPointTemp, ipEndPointHum };
 ListenPorts lp = new(ipPoints);
