a8a54b7d27ce3898996167664ac5b6e1470486de
Emir Büçkün <emirbuckun@hotmail.com>
2024-01-05 12:16:12 +0300

    Temperature Sensor Program Arranged - Console title added - Error handling added for socket connection - Messages modified

--- RemoteSensingApp.SensorTemp/Program.cs
+++ RemoteSensingApp.SensorTemp/Program.cs
@@ -3,26 +3,37 @@ using System.Net.Sockets;
 using System.Text;
 
 // Set TCP connection
+Console.Title = "Temperature Sensor";
 IPEndPoint ipEndPoint = new(IPAddress.Parse("127.0.0.1"), 8081);
 using Socket client = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-await client.ConnectAsync(ipEndPoint);
 
-// Send message
-while (true)
+try
 {
-    // Create temperature data btw 20-30
-    Random random = new();
-    int temperature = random.Next(20, 30);
-    DateTime dateTime = DateTime.Now;
+    // Connect to the gateway
+    await client.ConnectAsync(ipEndPoint);
 
-    // Send temperature data
-    var message = "Temperature Sensor: " + temperature.ToString() + "°C Date: " + dateTime.ToString();
-    var messageBytes = Encoding.UTF8.GetBytes(message);
-    _ = await client.SendAsync(messageBytes, SocketFlags.None);
+    // Send message
+    while (true)
+    {
+        // Create temperature data btw 20-30
+        Random random = new();
+        string temperature = random.Next(20, 30).ToString();
+        string now = DateTime.Now.ToString();
 
-    // Print sent data
-    Console.WriteLine("Temperature Sensor Sent Data: " + message);
+        // Send temperature data
+        var message = $"TEMP | {temperature} | {now}";
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        _ = await client.SendAsync(messageBytes, SocketFlags.None);
 
-    // Wait for a second
-    Thread.Sleep(1000);
+        // Print sent data
+        Console.WriteLine($"Sent: {message}");
+
+        // Wait for a second
+        Thread.Sleep(1000);
+    }
+}
+catch (SocketException ex)
+{
+    Console.WriteLine("\n\tThere is a problem with the gateway connection." +
+                    $"\n\tPlease try again. Error Details: {ex.Message}.\n");
 }
